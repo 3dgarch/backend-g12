ce791e9cf3e76e8d6af48047c9806c3af7cbcddb
3dgarch <edgarchavezg93@gmail.com>
2023-07-15 16:48:37 -0500

    finalizacion

---
+++ Dia-1/03-operadores-artimeticos.py
@@ -0,0 +1,31 @@
+numero1 = 10
+numero2 = 20
+
+nombre = "Luis"
+ciudad = "Lima"
+
+# Suma
+print(numero1 + numero2)
+
+# Resta
+print(numero1 - numero2)
+
+# multiplicacion
+print(numero1 * numero2)
+
+# division
+print(numero1 / numero2)
+
+# Modulo
+print(numero1 % numero2)
+
+# Cociente
+print(numero1 // numero2)
+
+
+
+# concatenar
+print(nombre+ciudad)
+
+# n veces repetidos
+print(nombre * 2)
---
+++ Dia-1/ejercicios.py
@@ -0,0 +1,5 @@
+# validar si el nombre de usuario es eduardo y de nacionalidad peruana o colombiano
+nombre = 'eduardo'
+nacionalidad = 'venezolana'
+
+print(nombre == 'eduardo' and (nacionalidad == 'peruano' or nacionalidad == 'colombiana'))
